59732d5da64c6baa988b841db365f04692ac792a
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-04-01 11:18:12 +0200

    Added controller and view files for category

---
+++ NorthwindMVC/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.EntityModels;
+
+namespace NorthwindMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
