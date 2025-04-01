b72fcb9e10797e84831da20bae786eccd29c4217
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-04-01 11:05:45 +0200

    Added new user field and updated registration

--- CustomI.Sample.CustomUserManagement/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ CustomI.Sample.CustomUserManagement/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -56,6 +56,17 @@ namespace CustomI.Sample.CustomUserManagement.Areas.Identity.Pages.Account.Manag
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            /// 
+            [Display(Name = "User Picture")]
+            public byte[] Picture { get; set; }
+
+            [Required]
+            [StringLength(30,
+                ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+                MinimumLength = 6)]
+            [Display(Name = "Full Name")]
+            public string FullName { get; set; }
+
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
@@ -65,28 +76,20 @@ namespace CustomI.Sample.CustomUserManagement.Areas.Identity.Pages.Account.Manag
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var fullName = user.FullName;
+            var Picture = user.Picture;
 
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FullName = fullName,
+                Picture = Picture
             };
         }
 
         public async Task<IActionResult> OnGetAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await LoadAsync(user);
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -99,7 +102,22 @@ namespace CustomI.Sample.CustomUserManagement.Areas.Identity.Pages.Account.Manag
                 await LoadAsync(user);
                 return Page();
             }
-
+            var fullName = user.FullName;
+            if (Input.FullName != fullName)
+            {
+                user.FullName = Input.FullName;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Request.Form.Files.Count > 0)
+            {
+                IFormFile file = Request.Form.Files.FirstOrDefault();
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    user.Picture = dataStream.ToArray();
+                }
+                await _userManager.UpdateAsync(user);
+            }
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
             {
--- CustomI.Sample.CustomUserManagement/Migrations/20250401084330_Update-identity-user-fields.Designer.cs
+++ CustomI.Sample.CustomUserManagement/Migrations/20250401084330_Update-identity-user-fields.Designer.cs
@@ -4,6 +4,7 @@ using CustomI.Sample.CustomUserManagement.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CustomI.Sample.CustomUserManagement.Migrations
 {
     [DbContext(typeof(CustomISampleCustomUserManagementContext))]
-    partial class CustomISampleCustomUserManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20250401084330_Update-identity-user-fields")]
+    partial class Updateidentityuserfields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
